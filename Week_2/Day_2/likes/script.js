bce4425d34efa79578db077298c3f3f9fbd7024d
Scott-Castay <107782006+Scott-Castay@users.noreply.github.com>
2022-07-02 13:18:38 -0500

    commit

---
+++ Week_2/Day_2/likes/script.js
@@ -0,0 +1,31 @@
+
+var numA = 9
+var numB = 12
+var numC = 9
+
+function counter()
+ {
+    console.log(numA)
+    
+    numA++
+    document.querySelector("#counter").innerHTML = numA; 
+    
+
+}
+function counter2()
+ {
+    console.log(numB)
+    
+    numB++
+    document.querySelector("#counter2").innerHTML = numB ;
+
+}
+function counter3()
+ {
+    console.log(numC)
+
+    numC++ 
+    document.querySelector("#counter3").innerHTML = numC ;
+}
+
+
